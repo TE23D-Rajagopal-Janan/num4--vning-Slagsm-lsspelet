41507ed18b124958ec0ef2dfdd36727ab8e52057
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-09-16 09:53:46 +0200

    jlj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,23 @@
-﻿int hp = 100;
-int dmg = Random.Shared.Next(80);
+﻿int hp1 = 100;
+int hp2 = 100;
+int dmg1 = Random.Shared.Next(80);
+int dmg2 = Random.Shared.Next(80);
 string name1;
 string name2;
+string ans1;
 
 Console.WriteLine("Vad heter spelare 1");
 name1 = Console.ReadLine ();
 Console.WriteLine("Vad heter spelare 2");
 name2 = Console.ReadLine ();
 
+while (hp1 > 0) 
+{
 Console.WriteLine ($"{name1} attakerar {name2}  ");
+hp2 = dmg1 -Random.Shared.Next(80); 
+Console.WriteLine ($"{name1} Har {hp1} hp kvar medans {name2} har {hp2} kvar");
 
+}
 
 
 
