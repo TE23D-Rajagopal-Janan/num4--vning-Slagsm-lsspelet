b1ec32b5ef30ea56685d147495e2e68d21d62aca
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-09-16 09:37:10 +0200

    deda

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int hp = 100;
+int dmg = Random.Shared.Next(80);
+string name1;
+string name2;
+
+Console.WriteLine("Vad heter spelare 1");
+name1 = Console.ReadLine ();
+Console.WriteLine("Vad heter spelare 2");
+name2 = Console.ReadLine ();
+
+Console.WriteLine ($"{name1} attakerar {name2}  ");
+
+
+
+
+
+Console.ReadLine ();
