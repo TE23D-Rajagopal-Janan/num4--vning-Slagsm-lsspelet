bc1fd92a0dd01de83886b1c9d2bf0d8b2266f65c
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-09-20 10:55:55 +0200

    ijheöhqwf

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,25 +1,44 @@
-﻿int hp1 = 100;
+﻿using System.Runtime.Serialization;
+
+int hp1 = 100;
 int hp2 = 100;
-int dmg1 = Random.Shared.Next(80);
-int dmg2 = Random.Shared.Next(80);
-string name1;
-string name2;
-string ans1;
 
 Console.WriteLine("Vad heter spelare 1");
-name1 = Console.ReadLine ();
-Console.WriteLine("Vad heter spelare 2");
-name2 = Console.ReadLine ();
-
-while (hp1 > 0) 
+string name1 = Console.ReadLine();
+while (name1 == "") 
 {
-Console.WriteLine ($"{name1} attakerar {name2}  ");
-hp2 = dmg1 -Random.Shared.Next(80); 
-Console.WriteLine ($"{name1} Har {hp1} hp kvar medans {name2} har {hp2} kvar");
+    Console.WriteLine("Skriv in ditt namn/Write your name");
+    name1 = Console.ReadLine();
+}  
+Console.WriteLine("Vad heter spelare 1");
+string name2 = Console.ReadLine();
+while (name2 == "") 
+{
+    Console.WriteLine("Skriv in ditt namn/Write your name");
+    name2 = Console.ReadLine();
+}  
+Console.WriteLine($"Hej {name2} och {name1}");
+
+while (hp1 > 1 && hp2 > 1)
+{
+    Console.WriteLine($"{name1} attakerar {name2}  ");
+    int dmg1 = Random.Shared.Next(30);
+    hp2 -= dmg1;
+    Console.WriteLine($"{name1} Har {hp1} hp kvar medans {name2} har {hp2} kvar");
+    Console.ReadLine();
+
+int dmg2 = Random.Shared.Next(30);
+    hp1 = -dmg2;
+    Console.WriteLine($"{name2} Har {hp2} hp kvar medans {name1} har {hp1} kvar");
+    Console.ReadLine();
+}
+if (hp2 == 0) 
+{
+    Console.WriteLine($"(name1) har besegrat (name2)");
+}
+else if (hp1 == 0)
+{
+    Console.WriteLine($"(name2) har besegrat (name1)");
 
 }
-
-
-
-
-Console.ReadLine ();
+Console.ReadLine();
